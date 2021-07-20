0dfae1332799ea7fa3f4155b1666d3d70d5a2641
ezzik1 <exstreamm96@gmail.com>
2021-07-21 01:53:25 +0400

    feat: end task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let mass = str.split(''),
+      open = [],
+      close = [];
+
+  for (let i = 0; i<bracketsConfig.length;i++) {
+      open.push(bracketsConfig[i][0]);
+      close.push(bracketsConfig[i][1]);
+  }
+
+  for (let i = 0; i<mass.length;i++) {
+      for (let j=0; j<open.length;j++) {
+          if (mass[i] === open[j] && mass[i+1] === close[j]) {
+              mass.splice(i,2);
+              i = -1;
+          }
+      }
+  }
+
+  if (mass.length > 0) {
+      return false
+  } else {
+      return true
+  }
 }
